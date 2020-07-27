84b205f03ab403a0127f6e8a0946b5339795a452
헤원 <jhw510@hanmail.net>
2020-07-27 13:49:59 +0900

    200727

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from "./mainComponent/component/Home";
+
+function App() {
+  return (
+    <div>
+
+        <Home/>
+
+    </div>
+  );
+}
+
+export default App;
